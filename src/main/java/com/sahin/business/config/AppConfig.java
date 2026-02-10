855e9f55cef879f7a9793829bf37095726591968
Mehmet Şahin <mhmtsahiin@gmail.com>
2026-02-10 03:23:02 +0300

    feat: Implement AppConfig

---
+++ src/main/java/com/sahin/business/config/AppConfig.java
@@ -0,0 +1,54 @@
+package com.sahin.business.config;
+
+import com.sahin.business.entities.Employee;
+import com.sahin.business.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
+
+public class AppConfig {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider=new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+    @Bean
+    public UserDetailsService userDetailsService (){
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                Optional<Employee> optional = employeeRepository.findByUserName(username);
+                if (optional.isPresent()) {
+                    return optional.get();
+                }
+                return null;
+            }
+        };
+    }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    // bi işe yarayacak mı acaba
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+
+
+}
